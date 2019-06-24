c0663aa04ca1d124b819145aa8ff7fe73439fa02
blax <blax56@gmail.com>
2019-06-24 16:43:40 +0300

    Chat

--- docs/docs.helper.js
+++ docs/docs.helper.js
@@ -11,7 +11,7 @@ import "codemirror/mode/jsx/jsx"
 import VueLazyload from "vue-lazyload"
 import VueMoment from "vue-moment"
 import VModal from "vue-js-modal"
-import VueAutosize from "vue-autosize/src/index"
+import VueAutosize from "vue-autosize"
 
 Vue.config.productionTip = false
 Vue.mixin(statusLabels)
