e221c4eb66790262528d30a8dcf5dd7f1c361c98
Shvg <itdzuj@gmail.com>
2019-10-03 18:16:09 +0300

    fix header

--- src/patterns/McHeader/McHeaderPart/McHeaderPartCenter.vue
+++ src/patterns/McHeader/McHeaderPart/McHeaderPartCenter.vue
@@ -35,7 +35,7 @@
             :href="menuMainItem.href"
             :to="menuMainItem.to"
             variation="black-flat"
-            :exact="$route.name.includes('index-index')"
+            :exact="$route.name.includes('index')"
           >
             <McSvgIcon slot="icon-prepend" :name="menuMainItem.icon" />
             {{ menuMainItem.name }}
