b1303f60e72a17d034419e4851d52540882c4268
kirillidze <krs@mediacube.network>
2019-10-25 18:34:33 +0300

    refs #5110; исправлены иконкиб добавлен слот в хедер mc-wrap-collapse

--- src/patterns/McWrap/McWrapCollapse.vue
+++ src/patterns/McWrap/McWrapCollapse.vue
@@ -2,50 +2,53 @@
   <div class="mc-wrap-collapse" :class="classes">
     <div class="mc-wrap-collapse__inner">
       <mc-collapse @toggle="handleToggle">
-        <mc-grid-row slot="activator" justify="between" align="middle" :gutter-x="8" :wrap="false">
-          <mc-grid-col class="mc-wrap-collapse__header">
-            <slot name="head" />
+        <div class="mc-wrap-collapse__activator" slot="activator">
+          <mc-grid-row justify="between" align="middle" :gutter-x="8" :wrap="false">
+            <mc-grid-col class="mc-wrap-collapse__header">
+              <slot name="head" />
 
-            <div class="mc-wrap-collapse__buttons" v-if="canDelete || isDraggable">
-              <mc-button
-                v-if="canDelete"
-                variation="gray-dark-flat"
-                size="m-compact"
-                @click.stop="handleDelete"
-              >
-                <mc-svg-icon slot="icon-append" name="delete" />
-              </mc-button>
-              <mc-button
-                v-if="isDraggable"
-                variation="gray-dark-flat"
-                size="m-compact"
-                @click.stop="handleDrag"
-                v-drag
-              >
-                <mc-svg-icon name="drag" size="s" />
-              </mc-button>
-            </div>
-          </mc-grid-col>
+              <div class="mc-wrap-collapse__buttons" v-if="canDelete || isDraggable">
+                <mc-button
+                  v-if="canDelete"
+                  variation="gray-dark-flat"
+                  size="m-compact"
+                  @click.stop="handleDelete"
+                >
+                  <mc-svg-icon slot="icon-append" name="delete" />
+                </mc-button>
+                <mc-button
+                  v-if="isDraggable"
+                  variation="gray-dark-flat"
+                  size="m-compact"
+                  @click.stop="handleDrag"
+                  v-drag
+                >
+                  <mc-svg-icon name="drag" size="s" />
+                </mc-button>
+              </div>
+            </mc-grid-col>
 
-          <template v-if="$slots.body">
-            <mc-grid-col v-if="isWarning && !toggled">
-              <mc-svg-icon
-                v-if="isWarning && !toggled"
-                name="warning"
-                fill="rgb(244, 62, 62)"
-                size="s"
-              />
-            </mc-grid-col>
-            <mc-grid-col style="display: flex;" v-if="!(isWarning && !toggled)">
-              <mc-svg-icon
-                class="mc-wrap-collapse__icon-arrow"
-                name="arrow_drop_down"
-                :fill="arrowColor"
-                size="s"
-              />
-            </mc-grid-col>
-          </template>
-        </mc-grid-row>
+            <template v-if="$slots.body">
+              <mc-grid-col v-if="isWarning && !toggled">
+                <mc-svg-icon
+                  v-if="isWarning && !toggled"
+                  name="warning"
+                  fill="rgb(244, 62, 62)"
+                  size="s"
+                />
+              </mc-grid-col>
+              <mc-grid-col style="display: flex;" v-if="!(isWarning && !toggled)">
+                <mc-svg-icon
+                  class="mc-wrap-collapse__icon-arrow"
+                  name="arrow_drop_down"
+                  :fill="arrowColor"
+                  size="s"
+                />
+              </mc-grid-col>
+            </template>
+          </mc-grid-row>
+          <slot name="head-bottom" />
+        </div>
 
         <slot name="body" slot="body" />
       </mc-collapse>
@@ -60,10 +63,12 @@ import McGridCol from "../McGrid/McGridCol"
 import McGridRow from "../McGrid/McGridRow"
 import McSvgIcon from "../../elements/McSvgIcon"
 import McButton from "../../elements/McButton"
+import Spacing from "../../../docs/components/tokens/Spacing"
 
 export default {
   name: "McWrapCollapse",
   components: {
+    Spacing,
     McCollapse,
     McGridCol,
     McGridRow,
@@ -245,6 +250,9 @@ export default {
                  <span slot="head">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda commodi culpa fugiat. Accusantium alias assumenda blanditiis dignissimos ducimus eius esse est excepturi fugiat, fugit illum itaque iure laborum maxime mollitia natus nemo odit officia officiis perferendis perspiciatis provident quos, reiciendis repellendus repudiandae saepe sunt suscipit, tempore tenetur voluptate voluptatibus!
                  </span>
+                <span slot="head-bottom">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda commodi culpa fugiat.
+                </span>
                 <span slot="body">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta dolores excepturi id maiores minima, reiciendis!
                 </span>
