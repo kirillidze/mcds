20f7398ecc71b66317863bc34b53f1211169b57c
Shvg <itdzuj@gmail.com>
2019-10-28 19:29:39 +0300

    fix

--- src/elements/McCropper.vue
+++ src/elements/McCropper.vue
@@ -46,8 +46,8 @@ export default {
     cropImage() {
       this.$refs.cropper
         .getCroppedCanvas({
-          maxWidth: 500,
-          maxHeight: 500,
+          maxWidth: 650,
+          maxHeight: 650,
         })
         .toBlob(blob => {
           this.$emit("crop", blob)
