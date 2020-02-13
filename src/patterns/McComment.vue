5208a54a22a151240fef30d2606c2df375207b08
Shvg <itdzuj@gmail.com>
2020-02-13 16:06:47 +0300

    add McComment

---
+++ src/patterns/McComment.vue
@@ -0,0 +1,53 @@
+<template>
+  <mc-preview class="mc-comment">
+    <mc-avatar slot="left" :src="comment.avatar" size="s" lazy rounded />
+
+    <div slot="top" class="mc-comment__name">
+      <mc-title size="s" :ellipsis="false">{{ comment.name }}</mc-title>
+
+      <mc-date
+        date-size="xs"
+        format="YYYY-MM-DD HH:mm"
+        color="gray-dark"
+        :ellipsis="false"
+        :value="comment.date"
+        :default-icon="false"
+      />
+    </div>
+
+    <div slot="bottom" class="mc-comment__text">
+      <mc-title size="s" :ellipsis="false">{{ comment.reason }}</mc-title>
+    </div>
+  </mc-preview>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.mc-comment {
+  $block-name: &;
+
+  &__name {
+    .mc-title {
+      width: auto;
+      margin-right: $space-xs;
+    }
+  }
+
+  &__text {
+    padding: $space-s;
+    margin-top: $space-xs;
+    background-color: $color-hover-gray;
+    border-radius: 0 8px 8px 8px;
+  }
+}
+</style>
