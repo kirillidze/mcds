ef49c5cfa4c6dde9f5da25d4ff28bc6512d2aa08
egor <egor@pop-os.localdomain>
2020-09-02 11:52:42 +0300

    refs #26104;fix menu

--- src/patterns/McHeader/McHeader.vue
+++ src/patterns/McHeader/McHeader.vue
@@ -371,6 +371,7 @@ export default {
     },
 
     updateMainMenu(val) {
+      this.canUpdateMenuItem = true
       this.menuMainMutable = []
       this.menuHidden = []
       this.menuMainMutable = val
