645d45e3114259178904b39b76d753747c3e99d3
kirillidze <krs@mediacube.network>
2021-02-16 13:39:24 +0300

    edit table attrs

--- src/patterns/McVirtualTable/McVirtualTable.vue
+++ src/patterns/McVirtualTable/McVirtualTable.vue
@@ -1,27 +1,9 @@
 <template>
   <div :style="wrapperStyles">
     <component
-      class="mc-virtual-table"
-      id="vxe-table"
-      ref="xTable"
       :is="tag"
-      column-key
-      v-bind="$attrs"
+      v-bind="attrs"
       v-on="$listeners"
-      row-id="id"
-      highlight-hover-row
-      highlight-current-row
-      show-header-overflow="tooltip"
-      show-overflow="tooltip"
-      show-footer-overflow="tooltip"
-      auto-resize
-      :context-menu="tableMenu"
-      :class="classes"
-      :sync-resize="cardIsOpen"
-      :scroll-y="scrollY"
-      :show-footer="canShowFooter"
-      :footer-method="footerMethod"
-      :sort-config="{ remote: !nativeSort, showIcon: false, trigger: 'cell', orders: sortOrders }"
       @scroll="handleScroll"
       @context-menu-click="contextMenuClickEvent"
     >
@@ -223,6 +205,36 @@ export default {
     api() {
       return this.$refs.xTable
     },
+    attrs() {
+      return {
+        id: "vxe-table",
+        "column-key": true,
+        ref: "xTable",
+        "context-menu": this.tableMenu,
+        class: this.classes,
+        "sync-resize": this.cardIsOpen,
+        "scroll-y": this.scrollY,
+        "show-footer": this.canShowFooter,
+        "footer-method": this.footerMethod,
+        "sort-config": {
+          remote: !this.nativeSort,
+          showIcon: false,
+          trigger: "cell",
+          orders: this.sortOrders,
+        },
+        "tooltip-config": {
+          theme: "mcui-black",
+        },
+        "row-id": "id",
+        "highlight-hover-row": true,
+        "highlight-current-row": true,
+        "show-header-overflow": "tooltip",
+        "show-overflow": "tooltip",
+        "show-footer-overflow": "tooltip",
+        "auto-resize": true,
+        ...this.$attrs,
+      }
+    },
     canShowLoader() {
       return !this.scrollable && this.hasMore
     },
@@ -237,6 +249,7 @@ export default {
     },
     classes() {
       return {
+        "mc-virtual-table": true,
         "mc-virtual-table--open-card": this.cardIsOpen,
         "mc-virtual-table--clickable": this.$listeners["cell-click"],
       }
@@ -488,213 +501,213 @@ $vxe-table-header-background-color: $color-white;
 </style>
 
 <docs>
-    ```jsx
-    import _minBy from 'lodash/minBy'
-    import _sortBy from 'lodash/sortBy'
-    import { number } from "../../utils/filters"
+```jsx
+import _minBy from 'lodash/minBy'
+import _sortBy from 'lodash/sortBy'
+import { number } from "../../utils/filters"
 
-    let body = require('../../mocks/tableInfusersBody').default;
-    let bodyMapped = body.map((item, index) => {
-        return {
-            ...item,
-            avatar: item.image_small,
-            views_count: number(item.views_count, 0),
-            average_views_per_video: number(item.average_views_per_video, 0),
-            subscribers_count: number(item.subscribers_count, 0),
-            categories: item.categories.map(c => c.title).join(', '),
-            language: item.language.name,
-            country: item.country.name,
-            price: item.agency_channels.filter( item => item.type === 2 ).length ? number(_minBy(item.agency_channels.filter( item => item.type === 2 ), 'total').total, 0) + ' $' : null,
-        }
-    }).slice(0, 50);
+let body = require('../../mocks/tableInfusersBody').default;
+let bodyMapped = body.map((item, index) => {
+return {
+...item,
+avatar: item.image_small,
+views_count: number(item.views_count, 0),
+average_views_per_video: number(item.average_views_per_video, 0),
+subscribers_count: number(item.subscribers_count, 0),
+categories: item.categories.map(c => c.title).join(', '),
+language: item.language.name,
+country: item.country.name,
+price: item.agency_channels.filter( item => item.type === 2 ).length ? number(_minBy(item.agency_channels.filter( item => item.type === 2 ), 'total').total, 0) + ' $' : null,
+}
+}).slice(0, 50);
 
-    let sortBy = 'language';
-    let sortDescending = true;
+let sortBy = 'language';
+let sortDescending = true;
 
-    let sort = ({key, descending}) => {
-    alert(`Key: ${key}, Desc: ${descending}`)
-    sortBy = key
-    sortDescending = descending
-    }
+let sort = ({key, descending}) => {
+alert(`Key: ${key}, Desc: ${descending}`)
+sortBy = key
+sortDescending = descending
+}
 
-    let checkedItems = [];
-    let check = value => {
-    alert(`Values: ${value}`)
-    checkedItems = [...value];
-    }
+let checkedItems = [];
+let check = value => {
+alert(`Values: ${value}`)
+checkedItems = [...value];
+}
 
-    const tablePlaceholders = {
-        no_data: 'Данных вообще нет!',
-        all_loaded: 'Всё уже загружено',
-        menu: {
-            copy: "Скопировать данные ячейки",
-            open_in_new_tab: "Открыть в новой вкладке",
-            open_in_new_window: "Открыть в новом окне",
-        },
-    }
+const tablePlaceholders = {
+no_data: 'Данных вообще нет!',
+all_loaded: 'Всё уже загружено',
+menu: {
+copy: "Скопировать данные ячейки",
+open_in_new_tab: "Открыть в новой вкладке",
+open_in_new_window: "Открыть в новом окне",
+},
+}
 
-    function handleCellClassName({rowIndex}) {
-        return "mc-virtual-table-col--border-bottom"
-    }
-    let loading = false
-    function handleLoad() {
-        loading = true
-        setTimeout(() => {
-            //alert(' data loaded')
-            loading = false
-        }, 2000)
-    }
+function handleCellClassName({rowIndex}) {
+return "mc-virtual-table-col--border-bottom"
+}
+let loading = false
+function handleLoad() {
+loading = true
+setTimeout(() => {
+//alert(' data loaded')
+loading = false
+}, 2000)
+}
 
-    const sortNameMethod = (a, b) => a - b
+const sortNameMethod = (a, b) => a - b
 
-    function handleSorted({order}) {
-        console.log(order)
-    }
-    function cellClickEvent(opt) {
-        console.log(opt)
-    }
-    function selectChangeEvent({row, checked}) {
-        alert(`${row.title} ${checked}`)
-    }
+function handleSorted({order}) {
+console.log(order)
+}
+function cellClickEvent(opt) {
+console.log(opt)
+}
+function selectChangeEvent({row, checked}) {
+alert(`${row.title} ${checked}`)
+}
 
-    <div>
-        <mc-title :ellipsis="false">
-            More info:
-            <mc-button variation="blue-link" href="https://xuliangzhan.com/vxe-table">
-                https://xuliangzhan.com/vxe-table
-            </mc-button>,
-            <mc-button variation="blue-link" href="https://xuliangzhan.github.io/vxe-table">
-                https://xuliangzhan.github.io/vxe-table
-            </mc-button>
-        </mc-title>
+<div>
+    <mc-title :ellipsis="false">
+        More info:
+        <mc-button variation="blue-link" href="https://xuliangzhan.com/vxe-table">
+            https://xuliangzhan.com/vxe-table
+        </mc-button>,
+        <mc-button variation="blue-link" href="https://xuliangzhan.github.io/vxe-table">
+            https://xuliangzhan.github.io/vxe-table
+        </mc-button>
+    </mc-title>
 
-        <br />
-        <br />
+    <br />
+    <br />
 
-        <mc-virtual-table
-            ref="table"
-            height="500"
-            scrollable
-            stripe
-            border="outer"
-            component-tag="grid"
-            :items="bodyMapped"
-            :has-more="false"
-            :loading="loading"
-            :placeholders="tablePlaceholders"
-            :cell-class-name="handleCellClassName"
-            :checkbox-config="{labelField: 'user'}"
-            native-sort
-            @checkbox-change="selectChangeEvent"
-            @load="handleLoad"
-            @sort-change="handleSorted"
-            @cell-click="cellClickEvent"
-        >
-            <mc-virtual-table-col type="seq" min-width="60" fixed="left" align="right" has-border />
-            <mc-virtual-table-col :show-overflow="false" type="checkbox" fixed="left" width="25" />
-            <mc-virtual-table-col field="title" title="Канал" width="248" fixed="left">
-                <template v-slot="{ row }">
-                    <mc-preview>
-                        <mc-avatar-status slot="left" border-color="blue" dot-color="orange" lazy :src="row.avatar" size="s"/>
-                        <mc-grid-row style="height: 100%" slot="cell-right" :wrap="false" align="middle" :gutter-x="5">
-                            <mc-grid-col>
-                                <mc-tooltip size="s" placement="top" content="Редактировать">
-                                    <mc-button variation="blue-link" size="s-compact">
-                                        <mc-svg-icon slot="icon-append" name="create" size="xxs"/>
-                                    </mc-button>
-                                </mc-tooltip>
-                            </mc-grid-col>
-                            <mc-grid-col>
-                                <mc-tooltip size="s" placement="top" content="Копировать">
-                                    <mc-button variation="blue-link" size="s-compact">
-                                        <mc-svg-icon slot="icon-append" name="delete" size="xxs"/>
-                                    </mc-button>
-                                </mc-tooltip>
-                            </mc-grid-col>
-                        </mc-grid-row>
-                        <mc-title slot="top"> {{ row.title }} </mc-title>
-                    </mc-preview>
-                </template>
-                <template v-slot:right="{ row }">
-                    <mc-button style="margin-right: 4px;" variation="blue-link" size="s-compact">
-                        <mc-svg-icon slot="icon-append" name="create" size="xxs"/>
-                    </mc-button>
-                    <mc-button variation="blue-link" size="s-compact">
-                        <mc-svg-icon slot="icon-append" name="delete" size="xxs"/>
-                    </mc-button>
-                </template>
-            </mc-virtual-table-col>
-
-            <mc-virtual-table-col type="seq" fixed="left" min-width="5" has-border>
-                <template v-slot="{ row }">
-                    <mc-bage vertical-line variation="light-green" style="position: absolute; top: 0; left: 0; bottom: 0" />
-                </template>
-            </mc-virtual-table-col>
-
-            <mc-virtual-table-col field="user" title="Пользователь" min-width="200">
-                <template v-slot="{ row }">
-                    <mc-title v-if="row.id%2">Почтальон Печкин</mc-title>
-                    <mc-title v-else style="width: auto; max-width: 101%;">Клён кудрявый лист резной</mc-title>
-                </template>
-            </mc-virtual-table-col>
-
-            <mc-virtual-table-col
-                field="views_count"
-                title="Просмотры"
-                min-width="130"
-                align="right"
-                sortable
-                :sortMethod="sortNameMethod"
-            />
-
-            <mc-virtual-table-col field="roles" title="Роль" width="190">
-                <template v-slot="{ row }">
-                    <mc-stack :limit="1">
-                        <mc-chip variation="gray-dark-invert">Администратор</mc-chip>
-                        <mc-chip variation="gray-dark-invert">Петух</mc-chip>
-                        <mc-chip variation="gray-dark-invert">Лопух</mc-chip>
-                    </mc-stack>
-                </template>
-            </mc-virtual-table-col>
-
-            <mc-virtual-table-col field="channels" title="Канал" width="120">
-                <template v-slot="{ row }">
-                    <mc-stack :limit="3">
-                        <mc-avatar rounded lazy size="s" />
-                        <mc-avatar rounded lazy size="s" />
-                        <mc-avatar rounded lazy size="s" />
-                        <mc-avatar rounded lazy size="s" />
-                    </mc-stack>
-                </template>
-            </mc-virtual-table-col>
-
-            <mc-virtual-table-col field="status" title="Статус" min-width="150">
-                <template v-slot="{ row }">
-                    <mc-bage variation="red">Отклонен</mc-bage>
-                </template>
-            </mc-virtual-table-col>
-
-            <mc-virtual-table-col field="owner" title="Владелец" width="200">
-                <template v-slot="{ row }">
-                    <div style="display: flex; align-items: center; height: 100%;">
-                        <mc-field-text :name="`name-${row.id}`" placeholder="Владелец" />
-                    </div>
-                </template>
-            </mc-virtual-table-col>
-
-            <mc-virtual-table-col field="action" title="Действие" width="243" fixed="right">
-                <template v-slot="{ row }">
-                    <mc-grid-row justify="right" :wrap="false" align="middle" :gutter-x="5">
+    <mc-virtual-table
+        ref="table"
+        height="500"
+        scrollable
+        stripe
+        border="outer"
+        component-tag="grid"
+        :items="bodyMapped"
+        :has-more="false"
+        :loading="loading"
+        :placeholders="tablePlaceholders"
+        :cell-class-name="handleCellClassName"
+        :checkbox-config="{labelField: 'user'}"
+        native-sort
+        @checkbox-change="selectChangeEvent"
+        @load="handleLoad"
+        @sort-change="handleSorted"
+        @cell-click="cellClickEvent"
+    >
+        <mc-virtual-table-col type="seq" min-width="60" fixed="left" align="right" has-border />
+        <mc-virtual-table-col :show-overflow="false" type="checkbox" fixed="left" width="25" />
+        <mc-virtual-table-col field="title" title="Канал" width="248" fixed="left">
+            <template v-slot="{ row }">
+                <mc-preview>
+                    <mc-avatar-status slot="left" border-color="blue" dot-color="orange" lazy :src="row.avatar" size="s"/>
+                    <mc-grid-row style="height: 100%" slot="cell-right" :wrap="false" align="middle" :gutter-x="5">
                         <mc-grid-col>
-                            <mc-button size="s">Выплатить</mc-button>
+                            <mc-tooltip size="s" placement="top" content="Редактировать">
+                                <mc-button variation="blue-link" size="s-compact">
+                                    <mc-svg-icon slot="icon-append" name="create" size="xxs"/>
+                                </mc-button>
+                            </mc-tooltip>
                         </mc-grid-col>
                         <mc-grid-col>
-                            <mc-button variation="red" size="s">Отменить</mc-button>
+                            <mc-tooltip size="s" placement="top" content="Копировать">
+                                <mc-button variation="blue-link" size="s-compact">
+                                    <mc-svg-icon slot="icon-append" name="delete" size="xxs"/>
+                                </mc-button>
+                            </mc-tooltip>
                         </mc-grid-col>
                     </mc-grid-row>
-                </template>
-            </mc-virtual-table-col>
-        </mc-virtual-table>
-    </div>
-    ```
+                    <mc-title slot="top"> {{ row.title }} </mc-title>
+                </mc-preview>
+            </template>
+            <template v-slot:right="{ row }">
+                <mc-button style="margin-right: 4px;" variation="blue-link" size="s-compact">
+                    <mc-svg-icon slot="icon-append" name="create" size="xxs"/>
+                </mc-button>
+                <mc-button variation="blue-link" size="s-compact">
+                    <mc-svg-icon slot="icon-append" name="delete" size="xxs"/>
+                </mc-button>
+            </template>
+        </mc-virtual-table-col>
+
+        <mc-virtual-table-col type="seq" fixed="left" min-width="5" has-border>
+            <template v-slot="{ row }">
+                <mc-bage vertical-line variation="light-green" style="position: absolute; top: 0; left: 0; bottom: 0" />
+            </template>
+        </mc-virtual-table-col>
+
+        <mc-virtual-table-col field="user" title="Пользователь" min-width="200">
+            <template v-slot="{ row }">
+                <mc-title v-if="row.id%2">Почтальон Печкин</mc-title>
+                <mc-title v-else style="width: auto; max-width: 101%;">Клён кудрявый лист резной</mc-title>
+            </template>
+        </mc-virtual-table-col>
+
+        <mc-virtual-table-col
+            field="views_count"
+            title="Просмотры"
+            min-width="130"
+            align="right"
+            sortable
+            :sortMethod="sortNameMethod"
+        />
+
+        <mc-virtual-table-col field="roles" title="Роль" width="190">
+            <template v-slot="{ row }">
+                <mc-stack :limit="1">
+                    <mc-chip variation="gray-dark-invert">Администратор</mc-chip>
+                    <mc-chip variation="gray-dark-invert">Петух</mc-chip>
+                    <mc-chip variation="gray-dark-invert">Лопух</mc-chip>
+                </mc-stack>
+            </template>
+        </mc-virtual-table-col>
+
+        <mc-virtual-table-col field="channels" title="Канал" width="120">
+            <template v-slot="{ row }">
+                <mc-stack :limit="3">
+                    <mc-avatar rounded lazy size="s" />
+                    <mc-avatar rounded lazy size="s" />
+                    <mc-avatar rounded lazy size="s" />
+                    <mc-avatar rounded lazy size="s" />
+                </mc-stack>
+            </template>
+        </mc-virtual-table-col>
+
+        <mc-virtual-table-col field="status" title="Статус" min-width="150">
+            <template v-slot="{ row }">
+                <mc-bage variation="red">Отклонен</mc-bage>
+            </template>
+        </mc-virtual-table-col>
+
+        <mc-virtual-table-col field="owner" title="Владелец" width="200">
+            <template v-slot="{ row }">
+                <div style="display: flex; align-items: center; height: 100%;">
+                    <mc-field-text :name="`name-${row.id}`" placeholder="Владелец" />
+                </div>
+            </template>
+        </mc-virtual-table-col>
+
+        <mc-virtual-table-col field="action" title="Действие" width="243" fixed="right">
+            <template v-slot="{ row }">
+                <mc-grid-row justify="right" :wrap="false" align="middle" :gutter-x="5">
+                    <mc-grid-col>
+                        <mc-button size="s">Выплатить</mc-button>
+                    </mc-grid-col>
+                    <mc-grid-col>
+                        <mc-button variation="red" size="s">Отменить</mc-button>
+                    </mc-grid-col>
+                </mc-grid-row>
+            </template>
+        </mc-virtual-table-col>
+    </mc-virtual-table>
+</div>
+```
 </docs>
